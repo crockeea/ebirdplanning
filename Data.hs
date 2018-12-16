3406442b7ba707aeb38bcf033ba411699e3dfc63
Crockett <ericcro@amazon.com>
2018-12-15 17:28:13 -0800

    Improved processing

--- Data.hs
+++ Data.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module Data (nameMapping, readHistogram, Hotspot(..)) where
+module Data (nameMapping, readHistogram, Hotspot(..), hotspots) where
 
 import qualified Codec.Binary.UTF8.String as U
 import qualified Data.ByteString.Lazy as C
@@ -11,14 +11,13 @@ import Data.Tuple (swap)
 import Network.HTTP.Conduit
 import System.Directory (createDirectoryIfMissing, doesFileExist)
 import System.FilePath.Posix (pathSeparator)
-import System.IO.Unsafe (unsafePerformIO)
 import Text.Read
 
 pathSep :: String
 pathSep = [pathSeparator]
 
-nameMapping :: Map String String
-nameMapping = unsafePerformIO $ do
+nameMapping :: IO (Map String String)
+nameMapping = do
   let dir = "data"
       filePath = "idmap.txt"
       url = "https://ebird.org/GuideMe?reportType=location&bMonth" ++
@@ -91,7 +90,245 @@ parseHistLine str =
        let (birdname,vals) = splitAt (length ws - 48) ws
            bird = concat $ intersperse "-" birdname
            -- read the value for the first week of August
-           augVal = case readMaybe $ vals !! 28 of
+           augVal = case mapM readMaybe $ (vals !!) <$> [26,27,28,29,30] of
                       Nothing -> error $ str
-                      Just x -> x
+                      Just x -> maximum x
        in (bird,augVal)
+
+hotspots :: IO [Hotspot]
+hotspots = do
+  nameMap <- nameMapping
+  let nameToHS region hsName = 
+        if member hsName nameMap
+        then HS{code=nameMap!hsName,..}
+        else error hsName
+      regionList = [
+       ("neahBay", neahBay),
+       ("portAngeles", portAngeles),
+       ("lakeCrescent", lakeCrescent),
+       ("highOlympics", highOlympics),
+       ("portTownsend", portTownsend),
+       ("kitsapPeninsula", kitsapPeninsula),
+       ("whidbeyIsland", whidbeyIsland),
+       ("discoveryBay", discoveryBay),
+       ("dungeness", dungeness)]
+  return $ concatMap (\(str,lst) -> nameToHS str <$> lst) regionList
+
+neahBay :: [String]
+neahBay = [
+  "Cape Flattery",
+  "Cape Flattery Road--Hobuck Overlook",
+  "Neah Bay--Cape Loop Road",
+  "Shi Shi Beach",
+  "Makah Fish Hatchery",
+  "Tsoo-Yess Beach",
+  "Tsoo-Yess River Valley",
+  "Tsoo-Yess River Valley--Makah Passage Rd",
+  "Neah Bay--Wa'Atch Beach Drive",
+  "Hobuck Beach",
+  "Hobuck / Hobuck Loop / Wa'atch Peak Rds",
+  "Hobuck Lake",
+  "Neah Bay--Sewage Treatment Ponds (open most weekdays)",
+  "Wa'atch River Valley",
+  "Neah Bay--general area (*move to more refined location if appropriate)",
+  "Neah Bay--Bahokus Peak",
+  "Neah Bay--town and bay only",
+  "Neah Bay--Ba'adah Village Loop Road",
+  "Neah Bay--Greenhouse seawatch",
+  "Neah Bay--housing development/clearcuts",
+  "Neah Bay—The Wedge",
+  "Snow Creek",
+  "Bullman Beach",
+  "Shipwreck Point",
+  "Sekiu River mouth",
+  "Hoko River SP--Cowan Ranch Heritage Site",
+  "Sekiu",
+  "Middle Point",
+  "Clallam Bay County Park",
+  "Clallam Bay",
+  "Lake Pleasant Recreation Area",
+  "Sol Duc Hatchery",
+  "Bear Creek Campgrond",
+  "Beaver Lake (Clallam Co.)",
+  "Pysht River Conservation Area",
+  "Pillar Point Recreation Area"]
+
+portAngeles :: [String]
+portAngeles = [
+  "Elwha River Mouth -- west access",
+  "Elwha River mouth",
+  "Elwha River Mouth -- east access",
+  "Port Angeles Landfill",
+  "Lincoln Park (Port Angeles)",
+  "Ediz Hook",
+  "Port Angeles waterfront",
+  "Valley Creek Estuary Park",
+  "Port Angeles City Pier",
+  "Olympic Discovery Trail - PA City Pier to Ennis Creek",
+  "Francis Street Park",
+  "Webster Park",
+  "Olympic NP--Headquarters"]
+
+lakeCrescent :: [String]
+lakeCrescent = [
+  "West Twin River mouth",
+  "Olympic National Park--Lake Crescent Campground",
+  "Olympic NF--Salmon Cascades",
+  "Lyre Conservation Area",
+  "Salt Creek County Park",
+  "Freshwater Bay",
+  "Lake Crescent",
+  "Olympic NP--Lake Crescent Lodge grounds and trails",
+  "Olympic NP--Lake Crescent--Marymere Falls Trail",
+  "Storm King Ranger station",
+  "Olympic NP--Sol Duc Hot Springs Resort",
+  "Sol Duc Campground",
+  "Olympic NP--Sol Duc Falls Trail"]
+
+highOlympics :: [String]
+highOlympics = [
+  "Olympic NP--Elwha area",
+  "Elwha River -- Aldwell area",
+  "Olympic NP--Hurricane Hill Trail",
+  "Olympic NP--Hurricane Ridge",
+  "Olympic NP--Meadow Loop Trail, Hurricane Ridge",
+  "Olympic NP--Overlook near Heart o' the Hills CG",
+  "Olympic NP -- Heart o' the Hills CG",
+  "Olympic NP--Morse Creek Overlook",
+  "Olympic NP--Deer Park",
+  "Olympic NP -- Obstruction Point Rd.",
+  "Blue Mountain"]
+
+dungeness :: [String]
+dungeness = [
+  "Olympic Peninsula--Sieberts Creek",
+  "Seibert Creek Conservation Area",
+  "Robin Hill Farm County Park",
+  "Kitchen-Dick Ponds",
+  "Olson Rd., Sequim",
+  "Dungeness Recreation Area",
+  "Dungeness NWR--forest trails",
+  "Dungeness NWR--Harbor side",
+  "Dungeness Spit--Strait of Juan de Fuca side",
+  "Dungeness NWR",
+  "Dungeness Bay",
+  "Dungeness--Corner of Lotzgesell and E.Anderson",
+  "Cline Spit",
+  "Lotzgesell Rd. Area (between Lotzgesell Rd., Cay's Rd., and E. A",
+  "Dungeness Landing Park/Oyster House",
+  "Lower Dungeness R. (Creamery) Trail",
+  "North Olympic WA--Lower Dungeness Unit",
+  "Three Crabs",
+  "Helen's Pond, Dungeness",
+  "Roberta's Pond, Dungeness",
+  "Gaskell Slough",
+  "Jamestown Beach",
+  "Graysmarsh",
+  "Sequim View Cemetery",
+  "Port Williams--Holland, Schmuck, John Scott Rds.",
+  "Port Williams & Schmuck Rds",
+  "Railroad Bridge Park",
+  "Lower Dungeness River Riparian Corridor",
+  "Marlyn Nelson County Park",
+  "Gibson Spit",
+  "Schmuck Road",
+  "Maple View Farm",
+  "Carrie Blake Park",
+  "Pacific Northwest National Laboratory",
+  "John Wayne Marina",
+  "Pitship Pocket Estuary",
+  "Sequim Bay State Park",
+  "Dungeness Forks Campground"]
+
+discoveryBay :: [String]
+discoveryBay = [
+  "Jimmy-Come-Lately Creek Estuary",
+  "Jimmy-come-lately Creek",
+  "Panorama Vista and Travis Spit",
+  "Miller Peninsula State Park and Thompson's Spit",
+  "Diamond Point",
+  "Thompson Road",
+  "Knapp Road (Clallam Co.)",
+  "Cat Lake Road",
+  "Miller Peninsula State Park",
+  "Gardiner Beach",
+  "Discovery Bay",
+  "Crocker Lake"]
+
+portTownsend :: [String]
+portTownsend = [
+  "Port Townsend -- Tibbals Lake Park",
+  "Port Townsend--Quimper Wildlife Corridor",
+  "Port Townsend -- North Beach",
+  "Fort Worden SP--Chinese Gardens Pond",
+  "Fort Worden SP",
+  "Port Townsend Marine Science Center",
+  "Point Wilson",
+  "Protection Island",
+  "Port Townsend--Glen Cove Sewage Pond",
+  "Port Townsend--Larry Scott Trail--bluff",
+  "Port Townsend--Larry Scott Trail - boat haven end",
+  "Kah Tai Lagoon",
+  "Port Townsend--Port of Port Townsend",
+  "Port Townsend ferry terminal",
+  "Port Townsend--Union Wharf",
+  "Chetzemoka Park",
+  "Point Hudson",
+  "Discovery Bay--Discovery Road Pond",
+  "Fort Townsend State Park",
+  "Kala Point",
+  "Anderson Lake SP",
+  "West Valley (Jefferson Co.)",
+  "Beaver Valley Rd, Chimacum, WA",
+  "HJ Carroll Park",
+  "Rick Tollefson Trail",
+  "Chimacum Creek",
+  "Irondale Beach Park",
+  "Port Hadlock--Ajax Cafe/NW School Boatbuilding",
+  "Hadlock Bay Road",
+  "Oak Bay County Park",
+  "Oak Bay--Lagoon Trail",
+  "Lagoon Beach",
+  "Lagoon Trail Park",
+  "Indian/Marrowstone Island isthmus",
+  "Nordland",
+  "East Beach Park, Marrowstone Island",
+  "Mystery Bay SP",
+  "Fort Flagler SP - Campground/Beach",
+  "Fort Flagler SP",
+  "Fort Flagler SP -- Marrowstone Point",
+  "Mats Mats Bay",
+  "Port Ludlow",
+  "Resort at Port Ludlow",
+  "Hood Canal--Paradise Bay",
+  "Hood Canal--Squamish Harbor",
+  "Shine Tidelands",
+  "Irondale Chimacum Creek Bay Park",
+  "Bywater Bay Spit & Tidelands"]
+
+kitsapPeninsula :: [String]
+kitsapPeninsula = [
+  "Salsbury Point Park",
+  "Kitsap Memorial SP",
+  "Port Gamble",
+  "Port Gamble Forest Heritage Park",
+  "Port Gamble Bay",
+  "Foulweather Bluff",
+  "Nature Conservancy Foulweather Bluff Preserve",
+  "Driftwood Key",
+  "Hansville Greenway--Lower Hawk's Pond Viewing Platform",
+  "Hansville Greenway",
+  "Buck Lake",
+  "Hansville",
+  "Norwegian Point",
+  "Point No Point",
+  "Port of Eglon",
+  "North Kitsap Heritage Park",
+  "Arness Roadside Park, Kingston",
+  "Kingston -- Village Green Comm Park",
+  "Kingston Ferry Terminal",
+  "Appletree Cove",
+  "Apple Cove Point"]
+
+whidbeyIsland :: [String]
+whidbeyIsland = []
--- Main.hs
+++ Main.hs
@@ -7,31 +7,15 @@ import Control.Arrow ((***))
 import Control.Monad (join)
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as C
-import Data.List (intersperse, sortOn, reverse, partition, stripPrefix, delete, filter, groupBy)
+import Data.List (intersperse, sortOn, reverse, partition, stripPrefix, delete, filter, groupBy, dropWhile, reverse)
 import Data.Map hiding (splitAt,drop,map,take,delete,filter,partition)
+import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.Tuple (swap)
 import GHC.IO.Encoding
+import Prelude hiding (lookup, null)
 import System.Directory (listDirectory)
 import System.IO
-
-hotspots :: [Hotspot]
-hotspots = 
-  let nameToHS region hsName = 
-        if member hsName nameMapping
-        then HS{code=nameMapping!hsName,..}
-        else error hsName
-      regionList = [
-       ("neahBay", neahBay),
-       ("portAngeles", portAngeles),
-       ("lakeCrescent", lakeCrescent),
-       ("highOlympics", highOlympics),
-       ("portTownsend", portTownsend),
-       ("kitsapPeninsula", kitsapPeninsula),
-       ("whidbeyIsland", whidbeyIsland),
-       ("discoveryBay", discoveryBay),
-       ("dungeness", dungeness)]
-  in concatMap (\(str,lst) -> nameToHS str <$> lst) regionList
 
 updateKeys :: (Ord k, Ord k') => (k -> k') -> Map k a -> Map k' a
 updateKeys f m = 
@@ -41,10 +25,14 @@ updateKeys f m =
 
 histograms :: IO (Map Hotspot (Map String Double))
 histograms = do
+  hss <- hotspots
   -- histpairs :: [(Hotspot,(Int,Map))]
-  histpairs <- zip hotspots <$> mapM readHistogram hotspots
-  let groupedHSs :: [[(Hotspot,(Int,Map String Double))]]
-      groupedHSs = groupBy (\a b -> region (fst a) == region (fst b)) histpairs
+  histpairs <- zip hss <$> mapM readHistogram hss
+  -- histograms include birds seen in *any* week of the year. Many have not been seen
+  -- in the first week of August, so let's remove them.
+  let filteredHSs = ((M.filter (/=0) <$>) <$>) <$> histpairs
+      groupedHSs :: [[(Hotspot,(Int,Map String Double))]]
+      groupedHSs = groupBy (\a b -> region (fst a) == region (fst b)) filteredHSs
       sortedGroups :: [([(Hotspot,(Int,Map String Double))],[(Hotspot,(Int,Map String Double))])]
       sortedGroups = partition ((< 5) . fst . snd) <$> groupedHSs
       printStats (a,b) = 
@@ -58,28 +46,74 @@ histograms = do
         join (***) (S.unions . map (keysSet . snd)) volHSs
       loVolOnly = loVolKeys S.\\ hiVolKeys
   putStrLn $ "Number of birds only seen at low-volume hotspots: " ++ show (length loVolOnly)
-  let loVolRare = map (flip restrictKeys loVolOnly <$>) loVolMaps
+  let loVolRare = filter (not . null . snd) $ map (flip restrictKeys loVolOnly <$>) loVolMaps
       -- adjust bird name to indicate a rarity
-      loVolRare' = (updateKeys ("RARE"++) <$>) <$> loVolRare
-  putStrLn $ show loVolRare
-  let largeMaps = concat $ snd <$> sortedGroups
+      loVolRare' = (updateKeys ("RARE: "++) <$>) <$> loVolRare
+  --    largeMaps = concat $ snd <$> sortedGroups
 
-  return $ fromList $ loVolRare' ++ hiVolMaps
+  return $ fromList $ hiVolMaps -- loVolRare' -- ++ 
 
 main :: IO ()
 main = do
-
   setLocaleEncoding utf8
-  _ <- histograms
+  hist <- histograms
+      -- already sorted using `toList`, so no need to resort. Just group.
+  let histGroups = groupWith (region . fst) $ toList hist :: [[(Hotspot,Map String Double)]]
+      compressRegion regMaps =
+        let regionRepr = fst $ head regMaps
+            mapsOnly = snd <$> regMaps
+        in (regionRepr, unionsWith max mapsOnly)
+      compressedHists = fromList $ compressRegion <$> histGroups
+  let str = showTable compressedHists
+  writeFile "out.txt" str
   putStrLn "done"
 
+invertMap :: forall a b . (Ord b) => Map a (Map b Double) -> Map b (Map a Double)
+invertMap m =
+  let allBs :: S.Set b
+      allBs = S.unions $ keysSet <$> m
+      indexb :: b -> Map b Double -> Double
+      indexb b m = case lookup b m of Nothing -> 0; (Just x) -> x
+      bmap :: b -> Map a Double
+      bmap b = indexb b <$> m
+  in fromSet bmap allBs
+
+-- one row of the table
+showRow :: (String,[(Hotspot, Double)]) -> String
+showRow (k,m) = concat $ intersperse "\t" $ k : (show <$> snd <$> m)
+
+hsAvg :: [(a,Double)] -> Double
+hsAvg xs = (sum $ snd <$> xs) / (fromIntegral $ length xs)
+
+-- you probably want to sort your input first!
+groupWith :: (Eq b) => (a -> b) -> [a] -> [[a]]
+groupWith f = groupBy (\a b -> f a == f b)
+
+sortAndGroup :: (Eq b, Ord b) => (a -> b) -> [a] -> [[a]]
+sortAndGroup f = groupWith f . sortOn f
+
+showTable :: Map Hotspot (Map String Double) -> String
+showTable m = 
+  let header = concat $ intersperse "\t" $ "" : (show <$> keys m)
+      invLists = toList <$> invertMap m :: Map String [(Hotspot,Double)]
+      -- remove 0s from each row, and sort from largest to smallest
+      invLists' = reverse <$> sortOn snd <$> (filter ((/= 0) . snd)) <$> invLists :: Map String [(Hotspot,Double)]
+      invLists'' = sortAndGroup (length . snd) $ toList invLists' -- :: [[(String,[(Hotspot,Double)])]]
+      sortedGroups = concat $ reverse $ sortOn (length . snd . head) $ reverse <$> sortOn (hsAvg . snd) <$> invLists''
+      dataStrs = showRow <$> sortedGroups
+  in concat $ intersperse "\n" $ header : dataStrs
+
+
+
+
+
+
 {-
-mapToFile :: Map String Double -> String
-mapToFile m = 
   let sortedData = reverse $ dropWhile ((<0.25) . snd) $ sortOn snd $ toList m
       strData = (show <$>) <$> sortedData
       birdLines = concat <$> intersperse "\t" <$> (\(a,b)->[a,b]) <$> strData
   in unlines birdLines
+
 
 -- process all hotspots in one region and report the maximum 
 -- probability of each bird at any of the hotspots, ignoring 
@@ -98,223 +132,3 @@ processArea hotspotIDs = do
   -- write the data to a file
   return finalMap
 -}
-
-
-neahBay :: [String]
-neahBay = [
-  "Cape Flattery",
-  "Cape Flattery Road--Hobuck Overlook",
-  "Neah Bay--Cape Loop Road",
-  "Shi Shi Beach",
-  "Makah Fish Hatchery",
-  "Tsoo-Yess Beach",
-  "Tsoo-Yess River Valley",
-  "Tsoo-Yess River Valley--Makah Passage Rd",
-  "Neah Bay--Wa'Atch Beach Drive",
-  "Hobuck Beach",
-  "Hobuck / Hobuck Loop / Wa'atch Peak Rds",
-  "Hobuck Lake",
-  "Neah Bay--Sewage Treatment Ponds (open most weekdays)",
-  "Wa'atch River Valley",
-  "Neah Bay--general area (*move to more refined location if appropriate)",
-  "Neah Bay--Bahokus Peak",
-  "Neah Bay--town and bay only",
-  "Neah Bay--Ba'adah Village Loop Road",
-  "Neah Bay--Greenhouse seawatch",
-  "Neah Bay--housing development/clearcuts",
-  "Neah Bay—The Wedge",
-  "Snow Creek",
-  "Bullman Beach",
-  "Shipwreck Point",
-  "Sekiu River mouth",
-  "Hoko River SP--Cowan Ranch Heritage Site",
-  "Sekiu",
-  "Middle Point",
-  "Clallam Bay County Park",
-  "Clallam Bay",
-  "Lake Pleasant Recreation Area",
-  "Sol Duc Hatchery",
-  "Bear Creek Campgrond",
-  "Beaver Lake (Clallam Co.)",
-  "Pysht River Conservation Area",
-  "Pillar Point Recreation Area"]
-
-portAngeles :: [String]
-portAngeles = [
-  "Elwha River Mouth -- west access",
-  "Elwha River mouth",
-  "Elwha River Mouth -- east access",
-  "Port Angeles Landfill",
-  "Lincoln Park (Port Angeles)",
-  "Ediz Hook",
-  "Port Angeles waterfront",
-  "Valley Creek Estuary Park",
-  "Port Angeles City Pier",
-  "Olympic Discovery Trail - PA City Pier to Ennis Creek",
-  "Francis Street Park",
-  "Webster Park",
-  "Olympic NP--Headquarters"]
-
-lakeCrescent :: [String]
-lakeCrescent = [
-  "West Twin River mouth",
-  "Olympic National Park--Lake Crescent Campground",
-  "Olympic NF--Salmon Cascades",
-  "Lyre Conservation Area",
-  "Salt Creek County Park",
-  "Freshwater Bay",
-  "Lake Crescent",
-  "Olympic NP--Lake Crescent Lodge grounds and trails",
-  "Olympic NP--Lake Crescent--Marymere Falls Trail",
-  "Storm King Ranger station",
-  "Olympic NP--Sol Duc Hot Springs Resort",
-  "Sol Duc Campground",
-  "Olympic NP--Sol Duc Falls Trail"]
-
-highOlympics :: [String]
-highOlympics = [
-  "Olympic NP--Elwha area",
-  "Elwha River -- Aldwell area",
-  "Olympic NP--Hurricane Hill Trail",
-  "Olympic NP--Hurricane Ridge",
-  "Olympic NP--Meadow Loop Trail, Hurricane Ridge",
-  "Olympic NP--Overlook near Heart o' the Hills CG",
-  "Olympic NP -- Heart o' the Hills CG",
-  "Olympic NP--Morse Creek Overlook",
-  "Olympic NP--Deer Park",
-  "Olympic NP -- Obstruction Point Rd.",
-  "Blue Mountain"]
-
-dungeness :: [String]
-dungeness = [
-  "Olympic Peninsula--Sieberts Creek",
-  "Seibert Creek Conservation Area",
-  "Robin Hill Farm County Park",
-  "Kitchen-Dick Ponds",
-  "Olson Rd., Sequim",
-  "Dungeness Recreation Area",
-  "Dungeness NWR--forest trails",
-  "Dungeness NWR--Harbor side",
-  "Dungeness Spit--Strait of Juan de Fuca side",
-  "Dungeness NWR",
-  "Dungeness Bay",
-  "Dungeness--Corner of Lotzgesell and E.Anderson",
-  "Cline Spit",
-  "Lotzgesell Rd. Area (between Lotzgesell Rd., Cay's Rd., and E. A",
-  "Dungeness Landing Park/Oyster House",
-  "Lower Dungeness R. (Creamery) Trail",
-  "North Olympic WA--Lower Dungeness Unit",
-  "Three Crabs",
-  "Helen's Pond, Dungeness",
-  "Roberta's Pond, Dungeness",
-  "Gaskell Slough",
-  "Jamestown Beach",
-  "Graysmarsh",
-  "Sequim View Cemetery",
-  "Port Williams--Holland, Schmuck, John Scott Rds.",
-  "Port Williams & Schmuck Rds",
-  "Railroad Bridge Park",
-  "Lower Dungeness River Riparian Corridor",
-  "Marlyn Nelson County Park",
-  "Gibson Spit",
-  "Schmuck Road",
-  "Maple View Farm",
-  "Carrie Blake Park",
-  "Pacific Northwest National Laboratory",
-  "John Wayne Marina",
-  "Pitship Pocket Estuary",
-  "Sequim Bay State Park",
-  "Dungeness Forks Campground"]
-
-discoveryBay :: [String]
-discoveryBay = [
-  "Jimmy-Come-Lately Creek Estuary",
-  "Jimmy-come-lately Creek",
-  "Panorama Vista and Travis Spit",
-  "Miller Peninsula State Park and Thompson's Spit",
-  "Diamond Point",
-  "Thompson Road",
-  "Knapp Road (Clallam Co.)",
-  "Cat Lake Road",
-  "Miller Peninsula State Park",
-  "Gardiner Beach",
-  "Discovery Bay",
-  "Crocker Lake"]
-
-portTownsend :: [String]
-portTownsend = [
-  "Port Townsend -- Tibbals Lake Park",
-  "Port Townsend--Quimper Wildlife Corridor",
-  "Port Townsend -- North Beach",
-  "Fort Worden SP--Chinese Gardens Pond",
-  "Fort Worden SP",
-  "Port Townsend Marine Science Center",
-  "Point Wilson",
-  "Protection Island",
-  "Port Townsend--Glen Cove Sewage Pond",
-  "Port Townsend--Larry Scott Trail--bluff",
-  "Port Townsend--Larry Scott Trail - boat haven end",
-  "Kah Tai Lagoon",
-  "Port Townsend--Port of Port Townsend",
-  "Port Townsend ferry terminal",
-  "Port Townsend--Union Wharf",
-  "Chetzemoka Park",
-  "Point Hudson",
-  "Discovery Bay--Discovery Road Pond",
-  "Fort Townsend State Park",
-  "Kala Point",
-  "Anderson Lake SP",
-  "West Valley (Jefferson Co.)",
-  "Beaver Valley Rd, Chimacum, WA",
-  "HJ Carroll Park",
-  "Rick Tollefson Trail",
-  "Chimacum Creek",
-  "Irondale Beach Park",
-  "Port Hadlock--Ajax Cafe/NW School Boatbuilding",
-  "Hadlock Bay Road",
-  "Oak Bay County Park",
-  "Oak Bay--Lagoon Trail",
-  "Lagoon Beach",
-  "Lagoon Trail Park",
-  "Indian/Marrowstone Island isthmus",
-  "Nordland",
-  "East Beach Park, Marrowstone Island",
-  "Mystery Bay SP",
-  "Fort Flagler SP - Campground/Beach",
-  "Fort Flagler SP",
-  "Fort Flagler SP -- Marrowstone Point",
-  "Mats Mats Bay",
-  "Port Ludlow",
-  "Resort at Port Ludlow",
-  "Hood Canal--Paradise Bay",
-  "Hood Canal--Squamish Harbor",
-  "Shine Tidelands",
-  "Irondale Chimacum Creek Bay Park",
-  "Bywater Bay Spit & Tidelands"]
-
-kitsapPeninsula :: [String]
-kitsapPeninsula = [
-  "Salsbury Point Park",
-  "Kitsap Memorial SP",
-  "Port Gamble",
-  "Port Gamble Forest Heritage Park",
-  "Port Gamble Bay",
-  "Foulweather Bluff",
-  "Nature Conservancy Foulweather Bluff Preserve",
-  "Driftwood Key",
-  "Hansville Greenway--Lower Hawk's Pond Viewing Platform",
-  "Hansville Greenway",
-  "Buck Lake",
-  "Hansville",
-  "Norwegian Point",
-  "Point No Point",
-  "Port of Eglon",
-  "North Kitsap Heritage Park",
-  "Arness Roadside Park, Kingston",
-  "Kingston -- Village Green Comm Park",
-  "Kingston Ferry Terminal",
-  "Appletree Cove",
-  "Apple Cove Point"]
-
-whidbeyIsland :: [String]
-whidbeyIsland = []
